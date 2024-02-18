22dbcf32241696ab9346d2c379ec7624a8ac565c
PsammystPrime <samuelprime384@gmail.com>
2024-02-19 01:08:34 +0300

    Add getting computer selection

--- javascript.js
+++ javascript.js
@@ -12,3 +12,20 @@ function userSelection(){
 userSelection()
 //buttons.forEach( (button) => addEventListener('click', console.log('click')) )
 
+function computerSelection () {
+    const choices = ['rock', 'paper', 'scissors']
+    let a = Math.random()
+    let b = Math.floor(a*10)
+  
+    if(b === 0 ||b ===  1 ||b ===  4 ||b ===  7 ){
+         c = 0
+    } else  if(b === 2 || b === 5 ||b ===  8 ){
+         c = 1
+    } else if(b === 3 ||b ===  6 ||b ===  9 ){
+        c = 2
+    }
+    let computer = choices[c]
+    console.log()
+    // rock.addEventListener('click', ))
+};
+
